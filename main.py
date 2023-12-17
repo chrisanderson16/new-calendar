33860b0084293a716ce24afba91dc86a0053b343
chrisanderson16 <anderson.chriz@gmail.com>
2023-12-17 15:29:24 -0400

    fixed?

--- main.py
+++ main.py
@@ -69,9 +69,55 @@ try:
 
     logging.info("Displaying day, month and date with thumbnail")
 
-    getAPI_data()
+
+#################################### A P I #####################################################
+
+# This is secret and you must get your own API key from the Nookipedia devs
+    api_key = os.environ["SECRET_NOOKIPEDIA_API_KEY"]
+
+# This will get the directory path of img/
+    dir_img = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'img')
+# This will list all the items in the img/ (AKA it will list all the files)
+    listdir_images = os.listdir(dir_img)
+
+# Gets the current date and time
+    date = datetime.datetime.now()
+# Month
+    currMonth = date.strftime("%B")
+# Date
+    currDate = date.strftime("%-d")
+
+
+# Starts the API session
+    villager_bday = villager_bday(api_key)
+
+# This calls the getCurrCharAll func in villager_bday class
+# It will give the character who's birthday it is today
+    parse_json_data = villager_bday.getCurrCharAll(currMonth, currDate, 'true')
+
+# Example of a day (2 characters)
+
+# This will give me the data I want for a given day
+#parse_json_data = villager_bday.getCurrCharAll('December', '5', 'true')
+# This prints the data to screen, or pass it to a text file to read easily
+    pp(parse_json_data)
+
+# This will return a list of names for a day
+    names = []
+    names = fromJSONgetName(parse_json_data)
+    print(names)
+
+# This will obtain the new image file.
+    getThumbnail(parse_json_data)
+
+
 
     
+    time.sleep(4)
+
+
+############################## CONVERTING IMG TO BMP ##############################################
+
 # Sets icon_file's path to be opened
     # This will open the first file it sees with 'img_'
     for item in os.listdir(dir_img):
@@ -110,7 +156,7 @@ try:
 
 
 
-    time.sleep(4)
+
 
     background_w_thumbnail_blk = Image.open(os.path.join(picdir, 'black_thumbnail.bmp'))
     canvas_red = Image.open(os.path.join(picdir, 'NULL_COLOUR.bmp'))

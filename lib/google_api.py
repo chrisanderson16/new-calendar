5b9de5451b9fd012b26d3e86c32afd8f2635f978
chrisanderson16 <anderson.chriz@gmail.com>
2023-12-18 12:48:27 -0400

    test

--- lib/google_api.py
+++ lib/google_api.py
@@ -19,7 +19,9 @@ def google_calendar_api(SCOPES):
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
-  if os.path.exists("lib/token.json"):
+
+
+  if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
   # If there are no (valid) credentials available, let the user log in.
   if not creds or not creds.valid:
@@ -27,11 +29,11 @@ def google_calendar_api(SCOPES):
       creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file(
-          "lib/credentials.json", SCOPES
+          "credentials.json", SCOPES
       )
       creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
-    with open("lib/token.json", "w") as token:
+    with open("token.json", "w") as token:
       token.write(creds.to_json())
 
   try:

77b45393afda600bdf9e73e5d052947962c0b6c5
chrisanderson16 <anderson.chriz@gmail.com>
2023-12-18 20:22:04 -0400

    test

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ try:
     # Individual calendar lines
     w_lines = 120
     for i in range(0,9):
-        draw_blk.line([(401,w_lines+i*40),(800,w_lines+i*40)], fill=None, width=0, joint=None)
+        draw_blk.line([(401,w_lines+i*40),(800,w_lines+i*40)], fill=None, width=1, joint=None)
     
     canvas_red.paste(calendar_icon(), (425,10))
     

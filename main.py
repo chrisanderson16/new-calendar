2b6dbfa7fd332e271c8ce75d4b2c587578ddfdb4
chrisanderson16 <anderson.chriz@gmail.com>
2023-12-18 22:39:24 -0400

    testings new lines

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ try:
     for i in range(0,9):
         draw_blk.line([(401,w_lines+i*40),(800,w_lines+i*40)], fill=None, width=1, joint=None)
 """
-    draw_blk.line([(401,120),(800,120)], fill=None, width=1, joint=None)
+    draw_blk.rectangle([(401,120),(800,120)], fill="grey", width=1, joint=None)
 # Google API
     cal_list = google_calendar_api(SCOPES)
 
